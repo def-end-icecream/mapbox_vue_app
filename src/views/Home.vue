9885653c64c1d366c2ef49d0c1af11c2eb22e0d9
dani <dani.zaghian@gmail.com>
2021-02-22 11:08:19 -0800

    adds marker and popup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,12 +16,21 @@
 export default {
   mounted: function() {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
-    new mapboxgl.Map({
+    var map = new mapboxgl.Map({
       container: 'map', // container id
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
       center: [-87.623177, 41.881832], // starting position [lng, lat]
       zoom: 11 // starting zoom
     });
+    // create the popup
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+    'This is Chicago, great!'
+    );
+    // create the marker
+    new mapboxgl.Marker()
+      .setLngLat([-87.623177, 41.881832])
+      .setPopup(popup) // sets a popup on this marker
+      .addTo(map);
   }
 };
 </script>
